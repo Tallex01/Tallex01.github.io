a74b165de83979a5b6f0607fd48ad77fb9886039
Alex Goodman <79115163+Tallex01@users.noreply.github.com>
2026-05-09 12:45:57 +0000

    links

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/resume-files", StaticFiles(directory="resume"), name="resume-files")
-
 
 
 
 
 
 app.mount("/", StaticFiles(directory="homepage", html=True), name="site")
+
+
+
